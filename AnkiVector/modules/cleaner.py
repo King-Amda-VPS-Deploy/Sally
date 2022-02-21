eb735e44cda60f127b2fd1b724f932608e1466e0
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-02-21 08:40:22 +0000

    Update cleaner.py

--- AnkiVector/modules/cleaner.py
+++ AnkiVector/modules/cleaner.py
@@ -263,7 +263,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "❗️Cleaning❗️"
+__mod_name__ = "🌹Cleaning🌹"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
