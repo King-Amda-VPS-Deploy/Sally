f4e5fb94a12e59fabc6306a1a94057cd82b85c74
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-02-21 07:03:31 +0000

    Add files via upload

---
+++ AnkiVector/modules/about.py
@@ -0,0 +1,13 @@
+__help__ = """
+** The Sally Botr - A Simple Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• Group Management Options With Ban , Kick  🚷
+• Songs Download 🎵
+
+Code From [Anki Vector Bot](https://github.com/Damantha126/The-Anki-Vector)
+
+**~ @NipunGroupBot**
+"""
+__mod_name__ = "♠️About♠️"
