de73fcd402030854d418e97135c956945c3ed3b0
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-02-21 08:42:59 +0000

    Update dev.py

--- AnkiVector/modules/dev.py
+++ AnkiVector/modules/dev.py
@@ -67,5 +67,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "🔰Dev🔰"
+__mod_name__ = "🌹Dev🌹"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER]
